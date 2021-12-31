aef70770134413f117d6bc5ec61da21bd5994bf6
harsh choubey <harshchoubey@harshs-MacBook-Pro.local>
2021-12-31 10:23:29 +0530

    Adding project to CucumberPOMSeriesTestNgSkipScenarios

--- src/test/java/parallel/ParallelRun.java
+++ src/test/java/parallel/ParallelRun.java
@@ -5,6 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+//checking
 @CucumberOptions(
 		plugin = {"pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
